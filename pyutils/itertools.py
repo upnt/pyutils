1de646b891cb285ad881c270828d5fa9ee7d377d
upnt <upnt.github@gmail.com>
2023-01-04 15:59:51 +0900

    add comment

--- pyutils/itertools.py
+++ pyutils/itertools.py
@@ -1,7 +1,6 @@
 from typing import Iterable, Callable
 import collections
 import copy
-import numpy as np
 
 
 def minmax(
@@ -15,7 +14,7 @@ def minmax(
         default: default value
 
     Returns:
-        any: _description_
+        (min_item, max_item): min and max items
     """
     min_val = default
     min_item = default
@@ -32,7 +31,15 @@ def minmax(
     return min_item, max_item
 
 
-def rotate(iterable: Iterable):
+def rotate(iterable: Iterable) -> Iterable[Iterable]:
+    """return iterables while moving the starting index
+
+    Args:
+        iterable: target iterable
+
+    Returns:
+        iterables: iterables with the different starting index
+    """
     deq = collections.deque(copy.deepcopy(iterable))
     yield iter(deq)
     for _ in range(len(deq) - 1):
@@ -40,26 +47,53 @@ def rotate(iterable: Iterable):
         yield iter(deq)
 
 
-def padding(iterable: Iterable, val=None, reverse=False):
-    max_length = len(max(iterable, key=len))
-    for item in iterable:
-        if len(item) < max_length:
-            if reverse:
-                item = [val] * (max_length - len(item)) + item
+def padding(iterables: Iterable[Iterable], value: any = None, is_forward: bool = False):
+    """make iterables of the same length by adding value
+
+    Args:
+        iterables: target iterables
+        value: padding value
+        is_forward: if is_forward is true, add value to front
+
+    Returns:
+        iterables: iterables of the same length
+    """
+    max_length = len(max(iterables, key=len))
+    for iterable in iterables:
+        if len(iterable) < max_length:
+            if is_forward:
+                iterable = [value] * (max_length - len(iterable)) + iterable
             else:
-                item = item + [val] * (max_length - len(item))
-        yield item
+                iterable = iterable + [value] * (max_length - len(iterable))
+        yield iterable
 
 
-def split(iterable: Iterable, sep=None, remove=True):
+def split(iterable: Iterable, sep: any = None, remove: bool = True):
+    """split iterable based on separator
+
+    Args:
+        iterable: target iterable
+        sep: separator
+        remove: if remove is true, removing separator from iterable
+
+    Returns:
+        iterables: splited iterables
+
+        input:
+            iterable: [0, 1, 2, 3, 2]
+            sep: 2
+            remove: True
+        
+        output:
+            iterables: [0, 1], [3], []
+    """
     result = []
-    line = []
     for val in iterable:
         if val == sep:
             if not remove:
-                line.append(val)
-            result.append(line)
-            line = []
+                result.append(val)
+            yield result
+            result = []
         else:
-            line.append(val)
-    return result
+            result.append(val)
+    yield result
