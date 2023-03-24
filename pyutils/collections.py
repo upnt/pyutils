71988c901ed4e560d06d1450e7042fe506437ae4
upnt <upnt.github@gmail.com>
2023-03-24 20:40:07 +0900

    create nddict

--- pyutils/collections.py
+++ pyutils/collections.py
@@ -2,28 +2,45 @@ import numpy as np
 
 
 class nddict:
-    def __init__(self, keys, key_dict, func=None):
-        self._keys = set()
-        self._key_dict = {}
-        self._array = None
+    def __init__(self, keys=None, values=None, /, func=None):
+        self._keys = keys
+        self._values = values
         self._func = lambda: None if func is None else func
 
-    def add_key(self, label, key_list):
-        if label in self._key_dict.keys():
-            raise ValueError(f"{label} is already exist")
-        for key in key_list:
-            if key in self._keys:
-                raise ValueError(f"{key} is already exist")
-        self._key_dict[label] = key_list
-        self._keys |= set(key_list)
-        if self._array is None:
-            self._array = [None for _ in key_list]
+    def add_key(self, key_list):
+        if self._keys is None:
+            self._keys = [key_list]
         else:
-            self._array = np.stack([np.full_like(self._array, self._func()) for _ in key_list], 0)
+            self._keys.append(key_list)
+
+        if self._values is None:
+            self._values = np.array([None for _ in key_list])
+        else:
+            self._values = np.stack([np.full_like(self._values, self._func()) for _ in key_list], axis=-1)
 
     def __getitem__(self, key):
-        for label, key_list in self._key_dict.items():
+        flag = False
+        for i, key_list in enumerate(self._keys):
             if key in key_list:
-                array_key = tuple(0 if i == label else slice(None, None, None) for i in self._key_dict)
-                return self._array[array_key]
-        raise ValueError(f"{key} is not in {self}")
+                major = i
+                miner = key_list.index(key)
+                flag = True
+                break
+        if not flag:
+            raise KeyError(f"{key} is not in {self._keys}")
+        keys = self._keys.copy()
+        del keys[major]
+        index = tuple(miner if i == major else slice(None, None, None) for i in range(self._values.ndim))
+        values = self._values[index]
+        if len(keys) == 0:
+            return values
+        else:
+            return nddict(keys, values, func=self._func)
+    
+    def __setitem__(self, key, value):
+        if not len(self._keys) == 1:
+            raise ValueError("{self.values} is not 1 dimentions")
+        key_list = self._keys[0]
+        if key not in key_list:
+            raise KeyError("{key} is not in {key_list}")
+        self._values[key_list.index(key)] = value
--- tests/test_collections.py
+++ tests/test_collections.py
@@ -1,13 +1,19 @@
 from pyutils.collections import nddict
 import numpy as np
+import itertools as itools
 def test_nddict():
-    dictionary = nddict()
-    dictionary.add_key("category", ["cat1", "cat2", "cat3"])
-    dictionary.add_key("pattern", ["pat1", "pat2", "pat3"])
-    dictionary.add_key("result", ["res1", "res2", "res3"])
-    assert np.all(dictionary._array == np.array([None for _ in range(3*3*3)]).reshape((3, 3, 3)))
-    dictionary._array = np.array(range(3*3*3)).reshape((3, 3, 3))
-    print(dictionary._array)
-    assert np.all(dictionary["cat1"] == np.array([[0, 1, 2], [3, 4, 5], [6, 7, 8]]))
-    assert np.all(dictionary["pat1"] == np.array([[0, 1, 2], [9, 10, 11], [18, 19, 20]]))
-    assert np.all(dictionary["res1"] == np.array([[0, 3, 6], [9, 12, 15], [18, 21, 24]]))
+    categories = ["cat1", "cat2", "cat3"]
+    patterns = ["pat1", "pat2", "pat3"]
+    results = ["res1", "res2", "res3"]
+    mdict = nddict()
+    mdict.add_key(categories)
+    mdict.add_key(patterns)
+    mdict.add_key(results)
+    mdict["cat1"]["pat2"]["res1"] = 0
+    assert mdict["cat1"]["pat2"]["res1"] == 0
+    
+    for cat, pat, res in itools.product(categories, patterns, results):
+        mdict[cat][pat][res] = f"{cat} {pat} {res}"
+    
+    assert mdict["cat1"]["pat2"]._values.tolist() == [f"cat1 pat2 {res}" for res in results]
+    assert mdict["pat2"]._values.flatten().tolist() == [f"{cat} pat2 {res}" for cat, res in itools.product(categories, results)]
