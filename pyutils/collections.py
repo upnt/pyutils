60352434cda9d443887b99d95a9175b7954b1c44
upnt <upnt.github@gmail.com>
2023-08-04 15:12:08 +0900

    fix bug

--- pyutils/collections.py
+++ pyutils/collections.py
@@ -2,12 +2,19 @@ import numpy as np
 
 
 class nddict:
-    def __init__(self, keys=None, values=None, /, func=None):
+    def __init__(self, keys, values, /, func=None):
         self._keys = keys
         self._values = values
         self._func = lambda: None if func is None else func
+    
+    @staticmethod
+    def from_keys(keys) -> "nddict":
+        result = nddict(None, None)
+        for key in keys:
+            result._add_key(key)
+        return result
 
-    def add_key(self, key_list):
+    def _add_key(self, key_list):
         if self._keys is None:
             self._keys = [key_list]
         else:
@@ -38,9 +45,18 @@ class nddict:
             return nddict(keys, values, func=self._func)
     
     def __setitem__(self, key, value):
-        if not len(self._keys) == 1:
-            raise ValueError(f"{self.values} is not 1 dimentions")
-        key_list = self._keys[0]
-        if key not in key_list:
-            raise KeyError(f"{key} is not in {key_list}")
-        self._values[key_list.index(key)] = value
+        if hasattr(key, '__iter__'):
+            key = tuple(key)
+        else:
+            key = (key,)
+        if len(key) != len(self._keys):
+                raise ValueError(f"Dimentions of {self._values} is smaller than {len(key)}")
+        index = []
+        for k, key_list in zip(key, self._keys):
+            if isinstance(k, slice):
+                index.append(k)
+            else:
+                if k not in key_list:
+                    raise KeyError(f"{k} is not in {key_list}")
+                index.append(key_list.index(k))
+        self._values[tuple(index)] = value
--- tests/test_collections.py
+++ tests/test_collections.py
@@ -1,19 +1,33 @@
 from pyutils.collections import nddict
 import numpy as np
 import itertools as itools
-def test_nddict():
+
+def test_create_nddict():
+    keys = [["cat1", "cat2"], ["pat1", "pat2", "pat3"]]
+    nd = nddict.from_keys(keys)
+    assert nd._values.shape == (2, 3)
+
+def test_nddict01():
+    keys = [["cat1", "cat2"], ["pat1", "pat2", "pat3"]]
+    mdict = nddict.from_keys(keys)
+    mdict["cat1", :] = 10
+    mdict["cat2"][1:2] = 20
+    expected = np.array([
+        [10, 10, 10],
+        [None, 20, None],
+    ])
+    assert np.all(mdict._values == expected)
+
+def test_nddict02():
     categories = ["cat1", "cat2", "cat3"]
     patterns = ["pat1", "pat2", "pat3"]
     results = ["res1", "res2", "res3"]
-    mdict = nddict()
-    mdict.add_key(categories)
-    mdict.add_key(patterns)
-    mdict.add_key(results)
-    mdict["cat1"]["pat2"]["res1"] = 0
+    mdict = nddict.from_keys([categories, patterns, results])
+    mdict["cat1", "pat2", "res1"] = 0
     assert mdict["cat1"]["pat2"]["res1"] == 0
     
     for cat, pat, res in itools.product(categories, patterns, results):
-        mdict[cat][pat][res] = f"{cat} {pat} {res}"
+        mdict[cat, pat, res] = f"{cat} {pat} {res}"
     
     assert mdict["cat1"]["pat2"]._values.tolist() == [f"cat1 pat2 {res}" for res in results]
     assert mdict["pat2"]._values.flatten().tolist() == [f"{cat} pat2 {res}" for cat, res in itools.product(categories, results)]
